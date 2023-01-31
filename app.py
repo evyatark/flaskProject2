c2bcf1ad9803f676ca3e369aa0d90df5044d13dd
evyatar <evyatar@tikalk.com>
2023-01-31 11:48:59 +0200

    add form for posts

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from markupsafe import escape
-from flask import render_template
+from flask import render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -21,7 +23,7 @@ def show_user_profile(username):
     return f'User {escape(username)}'
 
 
-@app.route('/post/<int:post_id>')
+@app.route('/aapost/<int:post_id>')
 def show_post(post_id):
     # show an example of receiving one parameter in the URL, the param is an integer
     return f'Post {post_id}'
@@ -55,10 +57,58 @@ def hello(name=None):
 
 
 
+@app.route('/index1')
+def index1():
+    return render_template('index1.html')
+
+
+posts = [{'id':0, 'title': 'First Post', 'content': 'Content for the first post', 'created': '2023-01-31 10:04'}]
+
+
+def get_post(post_id):
+    # conn = get_db_connection()
+    # post = conn.execute('SELECT * FROM posts WHERE id = ?',
+    #                     (post_id,)).fetchone()
+    # conn.close()
+    post = posts[post_id]
+    if post is None:
+        abort(404)
+    return post
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #posts = [{'id':1, 'title': 'First Post', 'content': 'Content for the first post', 'created': '10:04'}]
+    return render_template('index.html', posts=posts)
 
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        the_id = len(posts)
+        current_date_and_time = datetime.now()
+
+        if not title:
+            flash('Title is required!')
+        else:
+            # conn = get_db_connection()
+            # conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+            #              (title, content))
+            # conn.commit()
+            # conn.close()
+            posts.append({'id':the_id, 'title': title, 'content': content, 'created': current_date_and_time})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run()
